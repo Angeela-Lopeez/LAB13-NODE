551b37174d659ea54c414c4829db55c0b3053a4e
Angeela-Lopeez <angelalopezromero4@gmail.com>
2025-11-16 01:50:42 -0500

    5

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { createUser, findUserByEmail } from "@/lib/userStore";
+import { createUser, findUserByEmail } from "@/libTEMP/userStore";
 
 export async function POST(req: Request) {
   try {
